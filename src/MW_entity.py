ec320417553e56610d39ec903e20bbeab227d021
U-eddo-PC\eddo <eddo@eddo-PC.(none)>
2009-10-26 17:32:05 -0700

    todo

--- src/MW_entity.py
+++ src/MW_entity.py
@@ -22,7 +22,7 @@ class WallEn(Entity):
         Entity.__init__(self)
         self.pos = pos
         #self.image = pygame.image.load(os.path.join("data","basic_wall.png"))
-        #self.anim = MW_animator.Animator(MW_xml.getChildNodeWithAttribute(xml.dom.minidom.parse(os.path.join("data","tiles.xml")), "sprite","name","wall"))
+        self.anim = MW_animator.Animator(MW_xml.getChildNodeWithAttribute(xml.dom.minidom.parse(os.path.join("data","tiles.xml")), "sprite","name","wall"))
         self.state = "LIGHT"
         self.highlight = False
     def getName(self):
