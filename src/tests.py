36396bdf3edeac4b75cc53a79453084aac74f8d0
U-eddo-PC\eddo <eddo@eddo-PC.(none)>
2009-11-02 15:25:43 -0800

    cleaned up a bit

--- src/tests.py
+++ src/tests.py
@@ -1,5 +1,14 @@
 from distutils.core import setup
 import py2exe
+import os
+
+origIsSystemDLL = py2exe.build_exe.isSystemDLL
+def isSystemDLL(pathname):
+       if os.path.basename(pathname).lower() in ["sdl_ttf.dll"]:
+               return 0
+       return origIsSystemDLL(pathname)
+py2exe.build_exe.isSystemDLL = isSystemDLL
+
 setup(console=['main.py'])
 
 
